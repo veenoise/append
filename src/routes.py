17aefff981b0ddb511d6dcf09913cce49f6268ea
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-05-08 10:09:57 +0800

    add sample course page

--- src/routes.py
+++ src/routes.py
@@ -5,9 +5,16 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from src.db import get_db
 
+from markdown import markdown
+
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/courses/<coursecode>/<topic>')
+def course(coursecode, topic):
+    html = markdown(open(f'courses/{coursecode}/{topic}.md').read())
+    return render_template('course.html', course=html)
 
