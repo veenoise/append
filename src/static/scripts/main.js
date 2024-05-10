64d5b3b50ceff7e5aa6cf61fc13133a7cc411657
veenoise <william.eduard123@gmail.com>
2024-05-11 06:53:04 +0800

    Done with nav bar

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -5,3 +5,15 @@ document.querySelector('#menuButton').addEventListener('click', function() {
 document.querySelector('#closeMenu').addEventListener('click', function() {
   document.querySelector('.sidebar').style.display = "none";
 });
+
+// User icon iteraction
+let userIconClick = 0;
+document.querySelector('#userIcon').addEventListener('click', function() {
+  if (userIconClick % 2 === 0) {
+    document.querySelector('#userOptions').style.display = "block";
+  } else {
+    document.querySelector('#userOptions').style.display = "none";
+  }
+
+  userIconClick++;
+})
